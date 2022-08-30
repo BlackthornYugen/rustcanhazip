b340a15c02956f5c1cc5774a35c9f183dfcca9f7
John Steel <john@jskw.dev>
2022-08-30 10:09:01 -0400

    postman tests passing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #[macro_use] extern crate rocket;
 use std::net::IpAddr;
+use std::str;
+use std::process::Stdio;
+
+use dns_lookup::lookup_addr;
 
 use rocket::http::HeaderMap;
 use rocket::outcome::Outcome;
@@ -9,7 +13,6 @@ struct ClientData<'a> {
     headers: HeaderMap<'a>,
     ip: IpAddr,
 }
-
 
 #[rocket::async_trait]
 impl<'r> FromRequest<'r> for ClientData<'r> {
@@ -24,14 +27,44 @@ impl<'r> FromRequest<'r> for ClientData<'r> {
 }
 
 #[get("/")]
-fn index(data: ClientData) -> String {
+async fn index(data: ClientData<'_>) -> String {
     let host = data.headers.get_one("host").unwrap_or("");
     if host.contains("trace") {
-        data.ip.to_string() + "trace\n"
+        info!("TRACE: tracing {}...", data.ip);
+        let trace_text = trace(data.ip).await;
+        info!("TRACE: {} has been traced.", data.ip);
+        format!("{trace_text}\n")
     } else if host.contains("ptr") {
-        data.ip.to_string() + " ptr\n"
+        info!("PTR: looking up {}...", data.ip);
+        let address = lookup_addr(&data.ip).unwrap();
+        info!("PTR: {} resolved to {}", data.ip, address);
+        format!("{address}\n")
     } else {
-        data.ip.to_string() + "\n"        
+        info!("IP: {} returned", data.ip);
+        format!("{}\n", data.ip)
+    }
+}
+
+async fn trace(ip: IpAddr) -> String {
+    use std::process::Command;
+
+    let process = Command::new("sh") 
+        .arg("-c")
+        .arg(format!("traceroute {}", ip))
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .expect("failed to start process");
+
+    let output = process.wait_with_output().expect("process failed");
+
+    let stderr = str::from_utf8(&output.stderr).expect("can't process stderr");
+    let stdout = str::from_utf8(&output.stdout).expect("can't process stdout");
+
+    if output.status.success() {
+        return format!("{}{}", stderr, stdout)
+    } else {
+        return "".to_owned();
     }
 }
 
